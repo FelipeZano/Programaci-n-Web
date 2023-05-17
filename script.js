b584cd5bab5a27b3a1df10017629442f3f7a870c
FelipeZano <fzanelsuarez@itba.edu.ar>
2023-05-16 21:19:14 -0300

    clase  16/5

---
+++ script.js
@@ -0,0 +1,38 @@
+/*Indice mas alto = 2 osea que total de elementos es 3 */ 
+
+/*Arreis van siempre en Mayuscula  se hacen en const y se pueden modificar igual*/
+
+
+console.log("Hola")
+let array1 = [1,2,3]
+console.log(array1)
+console.log(array1[0]);
+
+const ARRAY2 = [1]
+//Metodos de arrays
+//Pop : Elimina el ultimo elemento
+
+array1.pop()
+
+//Push: añade un elemento al array
+
+array1.push(3,14)
+console.log(array1);
+
+//Shift: elimina el primer elemento del array
+//Unshift: agrega un elemento al principio del array
+
+console.log(array1[5]);
+
+// Map
+
+//Reduce: Aplica una operacion sobre toda una lista (Poco usable) 
+
+//indexof: Me indica en que posición esta el elemento que estoy buscando 
+console.log(array1.indexOf("Esteban"));
+
+//Find : Encuentra con conidciones
+
+
+
+//
