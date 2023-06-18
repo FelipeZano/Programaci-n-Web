46e22229d2abf90736bc524c7543488fd22b615a
FelipeZano <fzanelsuarez@itba.edu.ar>
2023-06-17 22:09:50 -0300

    Intento de Carrito

--- script.js
+++ script.js
@@ -30,15 +30,63 @@ console.log(array1[5]);
 
 //indexof: Me indica en que posición esta el elemento que estoy buscando 
 console.log(array1.indexOf("Esteban"));
-
+1
 //Find : Encuentra con conidciones
 
 //While-->
-const combo=["Combo", "250$", 400, 1]
-const spray=["Spray", "250$", 300, 1]
-const gotero=["Gotero", "250$", 200, 1]
+
+//Hasta aca son apuntes de clase-->
+
+// Desde aca hay u intento de generar elsubtotal
+
+const PrecioCombo=[400]
+const PrecioSpray=[300]
+const PrecioGotero=[200]
+
+const NUMCombo= document.getElementById("Cant.Combo")
+const NUMSpray= document.getElementById("Cant.Spray")
+const NUMGotero= document.getElementById("Cant.Gotero")
+
+let Subtotal = document.getElementById("Subtotal");
+
+function actualizarSubtotal() {
+    const CantidadCombo = parseInt(NUMCombo);
+    const CantidadSpray = parseInt(NUMSpray);
+    const CantidadGotero = parseInt(NUMGotero);
+
+    let Subtotal1 = [(PrecioCombo * CantidadCombo) + (PrecioSpray * CantidadSpray) + (PrecioGotero * CantidadGotero)];
+
+    //document.querySelector("Subtotal").innerHTML= Subtotal1
+    //  Subtotal.textContent = Subtotal1;
+}
+
+actualizarSubtotal()
+
+// Haasta aca hay u intento de generar el subtotal
 
 
+// Desde Aca le voy a dar funcion a los botones 
+//para que varien las canrtidades en la pagina
 
 
- 
+//Declaración de variables para el aumento
+const BotonSuma = document.getElementById("BotonSuma");
+const CantCombo = document.getElementById("CantCombo");
+
+let contador = 0 
+
+function BotonesSuma(){
+
+    contador++;
+    CantCombo.textContent = contador
+
+}
+
+BotonesSuma()
+
+
+BotonSuma.addEventListener("click", BotonesSuma)
+
+
+// HastaAca le voy a dar funcion a los botones 
+//para que varien las canrtidades en la pagina
